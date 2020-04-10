8482c82d3c51bb23991fe8c940515485d384564f
Jonny Baird <jonathan.baird@xdesign.com>
2020-04-10 15:50:10 +0100

    setting up project in xcode

---
+++ MyLocation/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyLocation
+//
+//  Created by Jonathan Baird  on 10/04/2020.
+//  Copyright © 2020 Jonathan Baird . All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
